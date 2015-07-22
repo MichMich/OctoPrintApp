af32221ef240fc17da4a6e863f88a0027d4543f9
Michael Teeuw <michael@xonaymedia.nl>
2015-07-22 15:16:01 +0200

    Initial Commit

---
+++ OctoPrint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OctoPrint
+//
+//  Created by Michael Teeuw on 22-07-15.
+//  Copyright © 2015 Michael Teeuw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ OctoPrintTests/OctoPrintTests.swift
@@ -0,0 +1,36 @@
+//
+//  OctoPrintTests.swift
+//  OctoPrintTests
+//
+//  Created by Michael Teeuw on 22-07-15.
+//  Copyright © 2015 Michael Teeuw. All rights reserved.
+//
+
+import XCTest
+@testable import OctoPrint
+
+class OctoPrintTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
