e677d6c223cf89e33e798d3dafaa66ac3168a73d
Michael Teeuw <michael@xonaymedia.nl>
2015-07-27 16:49:42 +0200

    Add images and split temperature from info

--- OctoPrint/OverviewTableViewController.swift
+++ OctoPrint/OverviewTableViewController.swift
@@ -11,15 +11,15 @@ import UIKit
 class OverviewTableViewController: UITableViewController {
 
     
-    let sections = ["Version", "State", "Temperatures"]
+    let sections = ["Version", "State"]
     
     override func viewDidLoad() {
+        title = "Info"
+        
         super.viewDidLoad()
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "updateUI", key: .DidUpdatePrinter, object: OPManager.sharedInstance)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "updateUI", key: .DidUpdateVersion, object: OPManager.sharedInstance)
-        
-        title = "OctoPrint"
         
         OPManager.sharedInstance.updateVersion()
         OPManager.sharedInstance.updatePrinter(autoUpdate:1)
@@ -48,8 +48,6 @@ class OverviewTableViewController: UITableViewController {
             case 1:
                 return 1
             
-            case 2:
-                return OPManager.sharedInstance.tools.count + 1
             
             default:
                 return 0
@@ -75,7 +73,6 @@ class OverviewTableViewController: UITableViewController {
 	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         
-        
         let shortPath = (indexPath.section, indexPath.row)
         switch shortPath {
             case (0, 0):
@@ -98,14 +95,6 @@ class OverviewTableViewController: UITableViewController {
                 cell.detailTextLabel?.text = OPManager.sharedInstance.printerStateText
                 cell.userInteractionEnabled = false
                 return cell
-            
-            case (2,_):
-                let cell = tableView.dequeueReusableCellWithIdentifier("TemperatureCell", forIndexPath: indexPath)
-                let tool = (indexPath.row == 0) ? OPManager.sharedInstance.bed : OPManager.sharedInstance.tools[indexPath.row - 1]
-
-                cell.textLabel?.text = tool.identifier
-                cell.detailTextLabel?.text = "\(tool.actualTemperature.celciusString()) (\(tool.targetTemperature.celciusString()))"
-                return cell
         
             default:
                 let cell = tableView.dequeueReusableCellWithIdentifier("BasicCell", forIndexPath: indexPath)
@@ -116,23 +105,11 @@ class OverviewTableViewController: UITableViewController {
 	}
 
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        if indexPath.section == 2 {
-            performSegueWithIdentifier("ShowTemperatureSelector", sender: self)
-        } else {
-            tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        }
+
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "ShowTemperatureSelector" {
-            if let indexPath = tableView.indexPathForSelectedRow {
-                let temperatureSelector = segue.destinationViewController as! TemperatureSelectorTableViewController
-                
-                temperatureSelector.heatedComponent = (indexPath.row == 0) ? OPManager.sharedInstance.bed : OPManager.sharedInstance.tools[indexPath.row - 1]
-                
-            }
-            
-        }
+
     }
 	
 }
--- OctoPrint/TemperatureTableViewController.swift
+++ OctoPrint/TemperatureTableViewController.swift
@@ -10,86 +10,109 @@ import UIKit
 
 class TemperatureTableViewController: UITableViewController {
 
+    
+    let sections = ["Bed", "Tools"]
+    
     override func viewDidLoad() {
+        title = "Temperature"
+        
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "updateUI", key: .DidUpdatePrinter, object: OPManager.sharedInstance)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "updateUI", key: .DidUpdateVersion, object: OPManager.sharedInstance)
+        
+        OPManager.sharedInstance.updateVersion()
+        OPManager.sharedInstance.updatePrinter(autoUpdate:1)
+        OPManager.sharedInstance.updateSettings()
+        
+        updateUI()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    
+    func updateUI() {
+        tableView.reloadData()
     }
-
-    // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return sections.count
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        switch section {
+        case 0:
+            return 1
+            
+        case 1:
+            return OPManager.sharedInstance.tools.count
+            
+        default:
+            return 0
+        }
+        
     }
-
-    /*
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section]
+    }
+    
+    override func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        if section == tableView.numberOfSections - 1 {
+            if let updated = OPManager.sharedInstance.updateTimeStamp {
+                let formattedDate = NSDateFormatter.localizedStringFromDate(updated,dateStyle: NSDateFormatterStyle.MediumStyle, timeStyle: .MediumStyle)
+                return "Last update: \(formattedDate)"
+            }
+        }
+        return nil
+    }
+    
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        
+        
+        
+        let shortPath = (indexPath.section, indexPath.row)
+        switch shortPath {
+        case (0,_):
+            let cell = tableView.dequeueReusableCellWithIdentifier("TemperatureCell", forIndexPath: indexPath)
+            let tool = OPManager.sharedInstance.bed
+            
+            cell.textLabel?.text = tool.identifier
+            cell.detailTextLabel?.text = "\(tool.actualTemperature.celciusString()) (\(tool.targetTemperature.celciusString()))"
+            return cell
+            
+        case (1,_):
+            let cell = tableView.dequeueReusableCellWithIdentifier("TemperatureCell", forIndexPath: indexPath)
+            let tool = OPManager.sharedInstance.tools[indexPath.row]
+            
+            cell.textLabel?.text = tool.identifier
+            cell.detailTextLabel?.text = "\(tool.actualTemperature.celciusString()) (\(tool.targetTemperature.celciusString()))"
+            return cell
+            
+        default:
+            let cell = tableView.dequeueReusableCellWithIdentifier("BasicCell", forIndexPath: indexPath)
+            cell.textLabel?.text = "Unknown cell!"
+            return cell
+        }
+        
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        performSegueWithIdentifier("ShowTemperatureSelector", sender: self)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ShowTemperatureSelector" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let temperatureSelector = segue.destinationViewController as! TemperatureSelectorTableViewController
+                
+                temperatureSelector.heatedComponent = (indexPath.section == 0) ? OPManager.sharedInstance.bed : OPManager.sharedInstance.tools[indexPath.row]
+                
+            }
+            
+        }
     }
-    */
 
 }
