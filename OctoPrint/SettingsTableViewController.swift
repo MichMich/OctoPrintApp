4478f3a92d1600e3428bfc6487153378eafa1e90
Michael Teeuw <michael@xonaymedia.nl>
2015-07-29 20:17:00 +0200

    Add QR scanner for API key in settings.

--- OctoPrint/SettingsTableViewController.swift
+++ OctoPrint/SettingsTableViewController.swift
@@ -7,16 +7,21 @@
 //
 
 import UIKit
+import AVFoundation
 
-class SettingsTableViewController: UITableViewController,UITextFieldDelegate {
+class SettingsTableViewController: UITableViewController, UITextFieldDelegate, QRCodeReaderViewControllerDelegate {
     
     @IBOutlet var hostTextField: UITextField!
     @IBOutlet var apiKeyTextField: UITextField!
+    
+    lazy var reader = QRCodeReaderViewController(metadataObjectTypes: [AVMetadataObjectTypeQRCode])
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Settings"
 
-        self.hostTextField.text = NSUserDefaults.standardUserDefaults().stringForKey("OctoPrintHost")
+        self.hostTextField.text = NSUserDefaults.standardUserDefaults().stringForKey("OctoPrintHost") ?? "octopi.local"
         self.apiKeyTextField.text = NSUserDefaults.standardUserDefaults().stringForKey("OctoPrintAPIKey")
     }
 
@@ -37,4 +42,38 @@ class SettingsTableViewController: UITableViewController,UITextFieldDelegate {
         return true
     }
 
+    @IBAction func scanButtonTapped(sender: AnyObject) {
+        if QRCodeReader.supportsMetadataObjectTypes() {
+            reader.modalPresentationStyle = .FormSheet
+            reader.delegate               = self
+            
+            reader.completionBlock = { (result: String?) in
+                print("Completion with result: \(result)")
+            }
+            
+            presentViewController(reader, animated: true, completion: nil)
+        }
+        else {
+            let alert = UIAlertController(title: "Whoops!", message: "Scanner not supported by the current device.", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
+            
+            presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
+    
+  
+    
+    // MARK: - QRCodeReader Delegate Methods
+    
+    func reader(reader: QRCodeReaderViewController, didScanResult result: String) {
+        self.dismissViewControllerAnimated(true, completion: { [unowned self] () -> Void in
+                self.apiKeyTextField.text = result
+        })
+    }
+    
+    func readerDidCancel(reader: QRCodeReaderViewController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
